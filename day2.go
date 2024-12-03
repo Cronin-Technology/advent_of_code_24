6ded07979328bb874a93bd93daece7c0d9f03962
jon-rebirtharmitage <jon@rebirtharmitage.com>
2024-12-02 22:06:57 -0600

    90% there with day 2 - moving on

--- day2.go
+++ day2.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"slices"
 	"strconv"
 	"strings"
 )
@@ -30,79 +31,101 @@ func main() {
 		a = append(a, b)
 	}
 
-	CheckReports(a)
+	//CheckReports(a)
 	// for f := range len(a) {
 	// 	fmt.Println(a[f])
 	// }
-}
 
-func CheckReports(a [][]int) {
-	total := 0
-	modTotal := 0
-	for i := range len(a) {
-		//direction := ""
-		count := 0
-		modCount := 0
-		if a[i][0]-a[i][1] < 0 {
-			//direction = "Increasing"
-			for j := range len(a[i]) - 1 {
-				if a[i][j]-a[i][j+1] < 0 && a[i][j]-a[i][j+1] > -4 && a[i][j]-a[i][j+1] != 0 {
-					count += 0
-				} else {
-					count += 1
-					if count > 0 && j+2 < len(a[i]) && a[i][j]-a[i][j+2] < 0 && a[i][j]-a[i][j+2] > -4 && a[i][j]-a[i][j+2] != 0 {
-						modCount += 1
-					}
-				}
-			}
-		} else if a[i][0]-a[i][1] > 0 {
-			//direction = "Decreasing"
-			for j := range len(a[i]) - 1 {
-				if a[i][j]-a[i][j+1] > 0 && a[i][j]-a[i][j+1] < 4 && a[i][j]-a[i][j+1] != 0 {
-					count += 0
-				} else {
-					count += 1
-					if count > 0 && j+2 < len(a[i]) && a[i][j]-a[i][j+2] > 0 && a[i][j]-a[i][j+2] < 4 && a[i][j]-a[i][j+2] != 0 {
-						modCount += 1
-					}
-				}
-			}
-		} else if a[i][0]-a[i][1] == 0 {
-			count += 1
-			if a[i][1]-a[i][2] < 0 {
-				//direction = "Increasing"
-				for j := range len(a[i]) - 1 {
-					if a[i][j]-a[i][j+1] < 0 && a[i][j]-a[i][j+1] > -4 && a[i][j]-a[i][j+1] != 0 {
-						count += 0
-					} else {
-						count += 1
-						if count > 0 && j+2 < len(a[i]) && a[i][j]-a[i][j+2] < 0 && a[i][j]-a[i][j+2] > -4 && a[i][j]-a[i][j+2] != 0 {
-							modCount += 1
-						}
-					}
-				}
-			} else if a[i][1]-a[i][2] > 0 {
-				//direction = "Decreasing"
-				for j := range len(a[i]) - 1 {
-					if a[i][j]-a[i][j+1] > 0 && a[i][j]-a[i][j+1] < 4 && a[i][j]-a[i][j+1] != 0 {
-						count += 0
-					} else {
-						count += 1
-						if count > 0 && j+2 < len(a[i]) && a[i][j]-a[i][j+2] > 0 && a[i][j]-a[i][j+2] < 4 && a[i][j]-a[i][j+2] != 0 {
-							modCount += 1
-						}
-					}
-				}
-			}
-		}
-		if count == 0 && modCount == 0 {
-			total += 1
-			//fmt.Println(a[i], " ", count, " direction: ", direction)
-		}
-		if modCount == 1 {
-			modTotal += 1
+	partone := 0
+	parttwo := 0
+
+	for i := range a {
+		if CheckData(a[i]) {
+			partone += 1
 		}
 	}
-	fmt.Println("Part 1: ", total)
-	fmt.Println("Part 2: ", modTotal, total+modTotal)
+
+	for i := range a {
+		if CheckDataTwice(a[i]) {
+			parttwo += 1
+		}
+	}
+
+	fmt.Println("Part 1: ", partone)
+	fmt.Println("Part 2: ", partone+parttwo)
+}
+
+func CheckDataAgain(data []int, index int) bool {
+	d := append([]int(nil), data...)
+	e := append([]int(nil), data...)
+	f := append([]int(nil), data...)
+	if index > 0 && index < len(data) {
+		d = slices.Delete(d, index, index+1)
+		e = slices.Delete(e, index+1, index+2)
+		f = slices.Delete(f, index-1, index)
+	} else if index == 0 {
+		d = slices.Delete(d, index, index+1)
+		e = slices.Delete(e, index+1, index+2)
+		f = slices.Delete(f, index, index+1)
+	} else if index == len(data)-1 {
+		d = slices.Delete(d, index-2, index-1)
+		e = slices.Delete(e, index-1, index)
+		f = slices.Delete(f, index-2, index-1)
+	}
+	if CheckData(d) || CheckData(e) || CheckData(f) {
+		return true
+	} else {
+		//fmt.Println(data, ":", d, ":", e, " ", index)
+		return false
+	}
+}
+
+func CheckDataTwice(data []int) bool {
+	count := 0
+	if data[0]-data[1] < 0 {
+		for j := 0; j < len(data)-1; j++ {
+			if data[j]-data[j+1] < 0 && data[j]-data[j+1] > -4 && data[j]-data[j+1] != 0 {
+				count += 0
+			} else if CheckDataAgain(data, j) {
+				return true
+			} else {
+				return false
+			}
+		}
+	} else if data[0]-data[1] > 0 {
+		for j := 0; j < len(data)-1; j++ {
+			if data[j]-data[j+1] > 0 && data[j]-data[j+1] < 4 && data[j]-data[j+1] != 0 {
+				count += 0
+			} else if CheckDataAgain(data, j) {
+				return true
+			} else {
+				return false
+			}
+		}
+	}
+	return false
+}
+
+func CheckData(data []int) bool {
+	count := 0
+	if data[0]-data[1] < 0 {
+		for j := 0; j < len(data)-1; j++ {
+			if data[j]-data[j+1] < 0 && data[j]-data[j+1] > -4 && data[j]-data[j+1] != 0 {
+				count += 0
+			} else {
+				return false
+			}
+		}
+		return true
+	} else if data[0]-data[1] > 0 {
+		for j := 0; j < len(data)-1; j++ {
+			if data[j]-data[j+1] > 0 && data[j]-data[j+1] < 4 && data[j]-data[j+1] != 0 {
+				count += 0
+			} else {
+				return false
+			}
+		}
+		return true
+	}
+	return false
 }
