b74925529a25bf91f6c22b340d787a6baefd9381
Jonathon Cronin <jon@rebirtharmitage.com>
2024-12-14 12:58:26 -0600

    Day14 Brutal Day - had to concede on part 1

--- day14.go
+++ day14.go
@@ -23,49 +23,98 @@ type robot struct {
 }
 
 const (
-	rows int = 7
-	cols int = 11
+	rows int = 103
+	cols int = 101
 )
 
+var robots = make(map[int]robot)
+
 func main() {
-	robots := []robot{}
-	readFile, err := os.Open("test.txt")
+	readFile, err := os.Open("day14.txt")
 	if err != nil {
 		fmt.Println(err)
 	}
 	fileScanner := bufio.NewScanner(readFile)
 	fileScanner.Split(bufio.ScanLines)
+	id := 0
 	for fileScanner.Scan() {
 		s := strings.Split(fileScanner.Text(), " ")
 		sx, sy := parseRobots(s[0])
 		vx, vy := parseRobots(s[1])
 		p := [][2]int{}
 		p = append(p, [2]int{sx, sy})
-		robots = append(robots, robot{sx, sy, vx, vy, p})
+		//robots = append(robots, robot{sx, sy, vx, vy, p})
+		robots[id] = robot{sx, sy, vx, vy, p}
+		id += 1
 	}
 
-	moveRobots(robots)
-	fmt.Println(robots)
+	for i := 0; i < 101; i++ {
+		moveRobots()
+	}
+	//fmt.Println(robots[1])
+	fmt.Println(countQuadrants())
 
 	readFile.Close()
 }
 
-func moveRobots(robots []robot) {
+func countQuadrants() int {
+	temp := [][2]int{}
+	for i := range len(robots) {
+		current := robots[i].position[len(robots[i].position)-1 : len(robots[i].position)]
+		update := current[0]
+		temp = append(temp, update)
+	}
+	//fmt.Println(temp)
+	q1 := 0
+	q2 := 0
+	q3 := 0
+	q4 := 0
+	for i := range len(temp) {
+		ignoreX := cols / 2
+		ignoreY := rows / 2
+		if temp[i][0] < ignoreX && temp[i][1] < ignoreY {
+			q1 += 1
+		} else if temp[i][0] > ignoreX && temp[i][1] < ignoreY {
+			q2 += 1
+		} else if temp[i][0] < ignoreX && temp[i][1] > ignoreY {
+			q3 += 1
+		} else if temp[i][0] > ignoreX && temp[i][1] > ignoreY {
+			q4 += 1
+		}
+	}
+	//return []int{q1, q2, q3, q4}
+	fmt.Println(q1, q2, q3, q4)
+	return q1 * q2 * q3 * q4
+}
+
+func moveRobots() {
 	for i := range len(robots) {
 		updateX, updateY := 0, 0
-		current := robots[i].position[:len(robots[i].position)]
-		if current[0][0]+robots[i].velocityX > 0 && current[0][0]+robots[i].velocityX <= cols {
+		current := robots[i].position[len(robots[i].position)-1 : len(robots[i].position)]
+		//fmt.Println("current : ", current)
+
+		if current[0][0]+robots[i].velocityX >= 0 && current[0][0]+robots[i].velocityX < cols {
 			updateX = current[0][0] + robots[i].velocityX
 		} else {
-			//teleport
+			if current[0][0]+robots[i].velocityX < 0 {
+				updateX = cols + (current[0][0] + robots[i].velocityX)
+			} else {
+				updateX = (current[0][0] + robots[i].velocityX) - cols
+			}
 		}
-		if current[0][1]+robots[i].velocityY > 0 && current[0][1]+robots[i].velocityY <= rows {
+		if current[0][1]+robots[i].velocityY >= 0 && current[0][1]+robots[i].velocityY < rows {
 			updateY = current[0][1] + robots[i].velocityY
 		} else {
-			//teleport
+			if current[0][1]+robots[i].velocityY < 0 {
+				updateY = rows + (current[0][1] + robots[i].velocityY)
+			} else {
+				updateY = (current[0][1] + robots[i].velocityY) - rows
+			}
 		}
 		update := [2]int{updateX, updateY}
-		robots[i].position = append(robots[i].position, update)
+		temp := robots[i].position
+		temp = append(temp, update)
+		robots[i] = robot{robots[i].startX, robots[i].startY, robots[i].velocityX, robots[i].velocityY, temp}
 	}
 }
 
--- day6.go
+++ day6.go
@@ -1,202 +1,202 @@
-package main
+// package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-	"slices"
-	"strings"
-)
+// import (
+// 	"bufio"
+// 	"fmt"
+// 	"os"
+// 	"slices"
+// 	"strings"
+// )
 
-type point struct {
-	value string
-	x     int
-	y     int
-	p     bool
-	d     []string
-	c     int
-}
-
-// var points = make(map[string]point)
-// var paradox = make(map[string]point)
-// var start []int
-// var direction string
-// var step int
-// var maxX int
-// var maxY int
-// var pointCount int
-
-func main() {
-	readFile, err := os.Open("test.txt")
-	if err != nil {
-		fmt.Println(err)
-	}
-	fileScanner := bufio.NewScanner(readFile)
-	y := 0
-	fileScanner.Split(bufio.ScanLines)
-	for fileScanner.Scan() {
-		s := strings.Split(fileScanner.Text(), "")
-		x := 0
-		for i := range len(s) {
-			a := makePoint(x, y)
-			b := point{s[i], x, y, false, []string{}, 0}
-			if s[i] == "^" {
-				z, e := getPoint(a)
-				start = append(start, z)
-				start = append(start, e)
-				direction = "up"
-			}
-			points[a] = b
-			paradox[a] = b
-			x += 1
-		}
-		maxX = x
-		y += 1
-	}
-	maxY = y
-
-	readFile.Close()
-	step = 0
-	GuardMovement()
-	Looper()
-	for i := range maxX {
-		//con := ""
-		for j := range maxY {
-			a := makePoint(j, i)
-			if points[a].value == "X" || points[a].value == "^" {
-				step += 1
-			}
-			//con += points[a].value
-		}
-		//fmt.Println(con)
-	}
-	fmt.Println("Part 1: ", step)
-
-	fmt.Println("Part 2: ", pointCount)
-}
-
-func GuardMovement() {
-	w := 0
-	next := []int{0, 0}
-	direction := []int{0, -1}
-	current := start
-	di := append(points[makePoint(current[0], current[1])].d, getDirection(direction))
-	points[makePoint(current[0], current[1])] = point{"X", points[makePoint(current[0], current[1])].x, points[makePoint(current[0], current[1])].y, true, di, points[makePoint(current[0], current[1])].c + 1}
-	for ok := true; ok; ok = (w != 2) { //do while loop
-		next[0] = (current[0] + direction[0])
-		next[1] = (current[1] + direction[1])
-		a := makePoint(next[0], next[1])
-		if points[a].value == "#" {
-			current[0] = (current[0] + (-1 * direction[0]))
-			current[1] = (current[1] + (-1 * direction[1]))
-			direction = turnRight(direction)
-		} else if next[0] > maxX || next[1] > maxY || next[0] < 0 || next[1] < 0 {
-			//Out of Bounds
-			w = 2
-		} else {
-			di := append(points[a].d, getDirection(direction))
-			points[a] = point{"X", points[a].x, points[a].y, true, di, points[a].c + 1}
-			current = next
-		}
-	}
-}
-
-func Looper() {
-	for i := range maxX {
-		//con := ""
-		for j := range maxY {
-			para := make(map[string]point)
-			for xx := range maxX {
-				//con := ""
-				for yy := range maxY {
-					a := makePoint(yy, xx)
-					para[a] = paradox[a]
-					//con += points[a].value
-				}
-				//fmt.Println(con)
-			}
-			a := makePoint(j, i)
-			if para[a].value != "^" || para[a].value != "#" {
-				para[a] = point{"#", para[a].x, para[a].y, false, []string{}, para[a].c}
-				if GuardMovementParadox(para) {
-					pointCount += 1
-				}
-				para = map[string]point{}
-			}
-			//con += points[a].value
-		}
-		//fmt.Println(con)
-	}
-}
-
-func GuardMovementParadox(para map[string]point) bool {
-	w := 0
-	l := 0
-	next := []int{0, 0}
-	direction := []int{0, -1}
-	current := start
-	di := append(para[makePoint(current[0], current[1])].d, getDirection(direction))
-	para[makePoint(current[0], current[1])] = point{"X", para[makePoint(current[0], current[1])].x, para[makePoint(current[0], current[1])].y, true, di, para[makePoint(current[0], current[1])].c + 1}
-	for ok := true; ok; ok = (w != 2) { //do while loop
-		next[0] = (current[0] + direction[0])
-		next[1] = (current[1] + direction[1])
-		a := makePoint(next[0], next[1])
-		if para[a].value == "#" {
-			current[0] = (current[0] + (-1 * direction[0]))
-			current[1] = (current[1] + (-1 * direction[1]))
-			direction = turnRight(direction)
-		} else if next[0] > maxX || next[1] > maxY || next[0] < 0 || next[1] < 0 {
-			//Out of Bounds
-			return false
-		} else {
-			if para[a].p {
-				if slices.Contains(para[a].d, getDirection(direction)) {
-					//fmt.Println(para[a].d, getDirection(direction))
-					return true
-				}
-			}
-			di := append(para[a].d, getDirection(direction))
-			para[a] = point{"X", para[a].x, para[a].y, true, di, para[a].c + 1}
-			current = next
-			l += 1
-		}
-	}
-	return false
-}
-
-func getDirection(d []int) string {
-	if d[0] == 0 && d[1] == -1 {
-		return "up"
-	} else if d[0] == 1 && d[1] == 0 {
-		return "right"
-	} else if d[0] == 0 && d[1] == 1 {
-		return "down"
-	} else if d[0] == -1 && d[1] == 0 {
-		return "left"
-	}
-	return "broken"
-}
-
-func turnRight(d []int) []int {
-	if d[0] == 0 && d[1] == -1 {
-		return []int{1, 0}
-	} else if d[0] == 1 && d[1] == 0 {
-		return []int{0, 1}
-	} else if d[0] == 0 && d[1] == 1 {
-		return []int{-1, 0}
-	} else if d[0] == -1 && d[1] == 0 {
-		return []int{0, -1}
-	}
-	return d
-}
-
-// func getPoint(a string) (int, int) {
-// 	b := strings.Split(a, ":")
-// 	bx, _ := strconv.Atoi(b[0])
-// 	by, _ := strconv.Atoi(b[1])
-// 	return bx, by
+// type point struct {
+// 	value string
+// 	x     int
+// 	y     int
+// 	p     bool
+// 	d     []string
+// 	c     int
 // }
 
-// func makePoint(x int, y int) string {
-// 	return (strconv.Itoa(x) + ":" + strconv.Itoa(y))
+// // var points = make(map[string]point)
+// // var paradox = make(map[string]point)
+// // var start []int
+// // var direction string
+// // var step int
+// // var maxX int
+// // var maxY int
+// // var pointCount int
+
+// func main() {
+// 	readFile, err := os.Open("test.txt")
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	}
+// 	fileScanner := bufio.NewScanner(readFile)
+// 	y := 0
+// 	fileScanner.Split(bufio.ScanLines)
+// 	for fileScanner.Scan() {
+// 		s := strings.Split(fileScanner.Text(), "")
+// 		x := 0
+// 		for i := range len(s) {
+// 			a := makePoint(x, y)
+// 			b := point{s[i], x, y, false, []string{}, 0}
+// 			if s[i] == "^" {
+// 				z, e := getPoint(a)
+// 				start = append(start, z)
+// 				start = append(start, e)
+// 				direction = "up"
+// 			}
+// 			points[a] = b
+// 			paradox[a] = b
+// 			x += 1
+// 		}
+// 		maxX = x
+// 		y += 1
+// 	}
+// 	maxY = y
+
+// 	readFile.Close()
+// 	step = 0
+// 	GuardMovement()
+// 	Looper()
+// 	for i := range maxX {
+// 		//con := ""
+// 		for j := range maxY {
+// 			a := makePoint(j, i)
+// 			if points[a].value == "X" || points[a].value == "^" {
+// 				step += 1
+// 			}
+// 			//con += points[a].value
+// 		}
+// 		//fmt.Println(con)
+// 	}
+// 	fmt.Println("Part 1: ", step)
+
+// 	fmt.Println("Part 2: ", pointCount)
 // }
+
+// func GuardMovement() {
+// 	w := 0
+// 	next := []int{0, 0}
+// 	direction := []int{0, -1}
+// 	current := start
+// 	di := append(points[makePoint(current[0], current[1])].d, getDirection(direction))
+// 	points[makePoint(current[0], current[1])] = point{"X", points[makePoint(current[0], current[1])].x, points[makePoint(current[0], current[1])].y, true, di, points[makePoint(current[0], current[1])].c + 1}
+// 	for ok := true; ok; ok = (w != 2) { //do while loop
+// 		next[0] = (current[0] + direction[0])
+// 		next[1] = (current[1] + direction[1])
+// 		a := makePoint(next[0], next[1])
+// 		if points[a].value == "#" {
+// 			current[0] = (current[0] + (-1 * direction[0]))
+// 			current[1] = (current[1] + (-1 * direction[1]))
+// 			direction = turnRight(direction)
+// 		} else if next[0] > maxX || next[1] > maxY || next[0] < 0 || next[1] < 0 {
+// 			//Out of Bounds
+// 			w = 2
+// 		} else {
+// 			di := append(points[a].d, getDirection(direction))
+// 			points[a] = point{"X", points[a].x, points[a].y, true, di, points[a].c + 1}
+// 			current = next
+// 		}
+// 	}
+// }
+
+// func Looper() {
+// 	for i := range maxX {
+// 		//con := ""
+// 		for j := range maxY {
+// 			para := make(map[string]point)
+// 			for xx := range maxX {
+// 				//con := ""
+// 				for yy := range maxY {
+// 					a := makePoint(yy, xx)
+// 					para[a] = paradox[a]
+// 					//con += points[a].value
+// 				}
+// 				//fmt.Println(con)
+// 			}
+// 			a := makePoint(j, i)
+// 			if para[a].value != "^" || para[a].value != "#" {
+// 				para[a] = point{"#", para[a].x, para[a].y, false, []string{}, para[a].c}
+// 				if GuardMovementParadox(para) {
+// 					pointCount += 1
+// 				}
+// 				para = map[string]point{}
+// 			}
+// 			//con += points[a].value
+// 		}
+// 		//fmt.Println(con)
+// 	}
+// }
+
+// func GuardMovementParadox(para map[string]point) bool {
+// 	w := 0
+// 	l := 0
+// 	next := []int{0, 0}
+// 	direction := []int{0, -1}
+// 	current := start
+// 	di := append(para[makePoint(current[0], current[1])].d, getDirection(direction))
+// 	para[makePoint(current[0], current[1])] = point{"X", para[makePoint(current[0], current[1])].x, para[makePoint(current[0], current[1])].y, true, di, para[makePoint(current[0], current[1])].c + 1}
+// 	for ok := true; ok; ok = (w != 2) { //do while loop
+// 		next[0] = (current[0] + direction[0])
+// 		next[1] = (current[1] + direction[1])
+// 		a := makePoint(next[0], next[1])
+// 		if para[a].value == "#" {
+// 			current[0] = (current[0] + (-1 * direction[0]))
+// 			current[1] = (current[1] + (-1 * direction[1]))
+// 			direction = turnRight(direction)
+// 		} else if next[0] > maxX || next[1] > maxY || next[0] < 0 || next[1] < 0 {
+// 			//Out of Bounds
+// 			return false
+// 		} else {
+// 			if para[a].p {
+// 				if slices.Contains(para[a].d, getDirection(direction)) {
+// 					//fmt.Println(para[a].d, getDirection(direction))
+// 					return true
+// 				}
+// 			}
+// 			di := append(para[a].d, getDirection(direction))
+// 			para[a] = point{"X", para[a].x, para[a].y, true, di, para[a].c + 1}
+// 			current = next
+// 			l += 1
+// 		}
+// 	}
+// 	return false
+// }
+
+// func getDirection(d []int) string {
+// 	if d[0] == 0 && d[1] == -1 {
+// 		return "up"
+// 	} else if d[0] == 1 && d[1] == 0 {
+// 		return "right"
+// 	} else if d[0] == 0 && d[1] == 1 {
+// 		return "down"
+// 	} else if d[0] == -1 && d[1] == 0 {
+// 		return "left"
+// 	}
+// 	return "broken"
+// }
+
+// func turnRight(d []int) []int {
+// 	if d[0] == 0 && d[1] == -1 {
+// 		return []int{1, 0}
+// 	} else if d[0] == 1 && d[1] == 0 {
+// 		return []int{0, 1}
+// 	} else if d[0] == 0 && d[1] == 1 {
+// 		return []int{-1, 0}
+// 	} else if d[0] == -1 && d[1] == 0 {
+// 		return []int{0, -1}
+// 	}
+// 	return d
+// }
+
+// // func getPoint(a string) (int, int) {
+// // 	b := strings.Split(a, ":")
+// // 	bx, _ := strconv.Atoi(b[0])
+// // 	by, _ := strconv.Atoi(b[1])
+// // 	return bx, by
+// // }
+
+// // func makePoint(x int, y int) string {
+// // 	return (strconv.Itoa(x) + ":" + strconv.Itoa(y))
+// // }
